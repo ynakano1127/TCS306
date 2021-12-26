fba95af04fa1711e6d21a00af8304d7bae92dbfb
ynakano1127 <ynakano1127@gmail.com>
2021-12-26 15:42:05 +0900

    [add]3

---
+++ 2/Env.java
@@ -0,0 +1,4 @@
+import java.util.HashMap;
+
+public class Env extends HashMap<String, Integer> {
+}
---
+++ 3/BinExprNode.java
@@ -0,0 +1,49 @@
+public class BinExprNode extends Node{
+    private Node left;
+    private  String op;
+    private Node right;
+    
+    public BinExprNode(int line, Node left, String op, Node right){
+        super(line);
+        this.left = left;
+        this.op = op;
+        this.right = right;
+    }
+
+    @Override
+    public int eval(Env env) throws ParseException {
+        switch (op) {
+            case "+":
+                return left.eval(env) + right.eval(env);
+            case "-":
+                return left.eval(env) - right.eval(env);
+            case "*":
+                return left.eval(env) * right.eval(env);
+            case "/":
+                return left.eval(env) / right.eval(env);
+            case ">":
+                return left.eval(env) > right.eval(env) ? 1 : 0;
+            case "<":
+                return left.eval(env) < right.eval(env) ? 1 : 0;
+            case ">=":
+                return left.eval(env) >= right.eval(env) ? 1 : 0;
+            case "<=":
+                return left.eval(env) <= right.eval(env) ? 1 : 0;
+            case "==":
+                return left.eval(env) == right.eval(env) ? 1 : 0;
+            case "!=":
+                return left.eval(env) != right.eval(env) ? 1 : 0;
+            case "&&":
+                return (left.eval(env)!=0) && (right.eval(env)!=0) ? 1 : 0;
+            case "||":
+                return (left.eval(env)!=0) || (right.eval(env)!=0) ? 1 : 0;
+            default:
+                throw new ParseException(op + "は未定義のオーペレーターです" + where());
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "(" + left + " " + op + " " + right + ")";
+    }
+}
